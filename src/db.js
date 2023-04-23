509b5b0ebafe4bcaea483eca8886e3030216dffc
alfredo0607 <alfredojosedominguezhernandez@gmail.com>
2023-04-23 11:33:07 -0500

    error de puesto, corregido

--- src/db.js
+++ src/db.js
@@ -7,7 +7,7 @@ const connection = mysql.createPool({
   user: process.env.DB_USER,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
-  port: "3306",
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   maxIdle: 10,
